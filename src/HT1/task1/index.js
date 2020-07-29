ee887f196a55a2a08315d062242e281e8ab16eff
Artsiom Papou <gefest_1@gefest.com>
2020-07-29 19:46:48 +0300

    added one more aproach to task1

--- src/HT1/task1/index.js
+++ src/HT1/task1/index.js
@@ -1,3 +1,6 @@
+/**
+ * Example 1 by readline. stdin\stout under the hood
+ */
 const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
@@ -8,3 +11,15 @@ rl.on('line', (input) => {
   console.log(input.split('').reverse().join(''));
 });
 rl.on('SIGINT', () => {rl.close()});
+
+/**
+ * Example 2 by stdin\stdout directly
+ */
+// process.stdin.resume();
+// process.stdin.on('data', (data) => {
+//   process.stdout.write(`${data.toString().split('').reverse().join('')}\n`);
+// });
+
+// process.on('SIGINT', () => {
+//   console.log('Received SIGINT.  Press Control-D to exit.');
+// });
--- src/HT1/task2/index.js
+++ src/HT1/task2/index.js
@@ -10,7 +10,7 @@
 import fs from 'fs';
 import csv from 'csvtojson';
 const csvFilePath='./src/HT1/task2/csv/example.csv';
-const readStream = fs.createReadStream(csvFilePath, {highWaterMark: 5});
+const readStream = fs.createReadStream(csvFilePath, {highWaterMark: 20});
 const writeStream = fs.createWriteStream('./src/HT1/task2/test.txt');
 
 readStream.on('error', error => console.error(error));
