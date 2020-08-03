f8f85b872ed5db9a80ce59889ad380978cdac6e2
Artsiom Papou <gefest_1@gefest.com>
2020-08-03 19:10:38 +0300

    task3 init

--- src/HT2/app.js
+++ src/HT2/app.js
@@ -1,10 +1,16 @@
 import express from 'express';
+import {client} from 'pg';
+import {DB_VARS} from './configs/db';
 import userRoute from './routes/user';
 import bodyParser from 'body-parser';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT);
+
+client.connect(DB_VARS.DATABASE_URL, (error, client, done) => {
+  console.log(client)
+});
 
 app.use(bodyParser.urlencoded({
   extended: false
---
+++ src/HT2/configs/db.js
@@ -0,0 +1,3 @@
+export const DB_VARS = {
+  DATABASE_URL: 'postgres://zosokpxwholzzf:c4024bc16c4fb5515a8cba83121667506f0cf6d704dca463c84f916e025d5a45@ec2-54-247-118-139.eu-west-1.compute.amazonaws.com:5432/d18ai4cvuvf7i8'
+};
